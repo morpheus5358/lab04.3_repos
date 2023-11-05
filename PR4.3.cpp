45084a47316b8df4082894d3f790d0a37c2b1065
Denys Martyniuk <146194242+morpheus5358@users.noreply.github.com>
2023-11-05 22:22:39 +0200

    Add project files.

---
+++ PR4.3.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+using namespace std;
+int main()
+{
+	double x, xp, xk, dx, a, b, c, F;
+	cout << "xp = "; cin >> xp;
+	cout << "xk = "; cin >> xk;
+	cout << "dx = "; cin >> dx;
+	cout << "a = "; cin >> a;
+	cout << "b = "; cin >> b;
+	cout << "c = "; cin >> c;
+	cout << fixed;
+	cout << "---------------------------" << endl;
+	cout << "|" << setw(5) << "x" << " |"
+		<< setw(7) << "F" << " |" << endl;
+	cout << "---------------------------" << endl;
+	x = xp;
+	while (x <= xk)
+	{
+		if (x < 0 && b != 0)
+			F = -a * pow(x, 2) + b;
+		else
+			if (x > 0 && b == 0)
+				F = (x / (x - c)) + 5.5;
+			else
+				F = x / -c;
+		cin.get();
+		cout << "|" << setw(7) << setprecision(2) << x
+			<< " |" << setw(10) << setprecision(3) << F
+			<< " |" << endl;
+		x += dx;
+	}
+	cout << "---------------------------" << endl;
+	return 0;
+}
